b38278500c0b7b7463bbb6c539cd8650ce7ce086
nbayoungnate4kt <nathanael134@hotmail.com>
2019-02-07 15:44:46 -0400

    added bootstrap

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
+import '../lib/collections.js';
 
 
 Template.profile.events({
@@ -11,5 +11,22 @@ Template.profile.events({
   },
   'click .js-dislike'(event, instance) {
     console.log("you click dislike");
+  },
+});
+
+Template.addProfile.events({
+	'click .js-saveProfile'(event, instance){
+	var fName = $("#exampleModal input[name='firstName']").val();
+	var lName = $("#exampleModal input[name='lastName']").val();
+	var pName = $("#exampleModal input[name='photoName']").val();
+	console.log("The First Name is",fName)
+	console.log("The Last Name is",lName)
+	console.log("The Photo Name is",pName)
+	//rest the form
+	$("#exampleModal input[name='firstName']").val('');
+	$("#exampleModal input[name='lastName']").val('');
+	$("#exampleModal input[name='photoName']").val('');
+	//close the modal
+  	$("#exampleModal").modal("hide");
   },
 });
