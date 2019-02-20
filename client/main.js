d564d5d98548f82933aa27fea8c609a8b5665531
nbayoungnate4kt <nathanael134@hotmail.com>
2019-02-20 15:33:55 -0400

    add new code

--- client/main.js
+++ client/main.js
@@ -5,8 +5,8 @@ import './main.html';
 import '../lib/collections.js';
 
 Template.profile.helpers({
-	proffname(){
-		return userDB.findOne({}).firstName;
+	profAll(){
+		return userDB.find({});
 	}
 });
 
@@ -17,6 +17,12 @@ Template.profile.events({
   'click .js-dislike'(event, instance) {
     console.log("you click dislike");
   },
+  'click .js-delete'(event, instance){
+  	var profID = this._id;
+  	$("#" + profID).fadeOut("slow", "swing", function () {
+  	userDB.remove({_id: profID});
+  });
+  }
 });
 
 Template.addProfile.events({
@@ -24,6 +30,9 @@ Template.addProfile.events({
 	var fName = $("#exampleModal input[name='firstName']").val();
 	var lName = $("#exampleModal input[name='lastName']").val();
 	var pName = $("#exampleModal input[name='photoName']").val();
+	if (pName == ""){
+		pName="logo2.jpg";
+	}
 	console.log("The First Name is",fName)
 	console.log("The Last Name is",lName)
 	console.log("The Photo Name is",pName)
